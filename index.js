51edd3f81604ed4959fd2a04d699446cd0ea0444
Anri <anri0806@hotmail.co.jp>
2021-09-11 16:39:55 -0400

    commit tree 92c4bc878e26271e8e67be72db7c857573ecd6af

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name) {
+    let newDrivers = array.filter(driver => 
+         driver.toLowerCase() === name.toLowerCase())
+    return newDrivers
+}
+
+function fuzzyMatch(array, name) {
+    let newDrivers = array.filter(driver =>
+        driver[0] === name[0])
+    return newDrivers
+}
+
+function matchName(array, name) {
+    let newDrivers = array.filter(driver =>
+        driver.name === name)
+    return newDrivers
+}
